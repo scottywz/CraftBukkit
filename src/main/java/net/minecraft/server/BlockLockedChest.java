5398fd9ac39f1e6fa2597d6b8eacc46e0b87f27a
Scott Zeid <s@zeid.me>
2013-08-03 00:18:33 -0500

    Fire LockedChestDecayEvent when a locked chest decays.  Adds BUKKIT-122

--- src/main/java/net/minecraft/server/BlockLockedChest.java
+++ src/main/java/net/minecraft/server/BlockLockedChest.java
@@ -2,6 +2,8 @@ package net.minecraft.server;
 
 import java.util.Random;
 
+import org.bukkit.event.block.LockedChestDecayEvent; // CraftBukkit
+
 public class BlockLockedChest extends Block {
 
     protected BlockLockedChest(int i) {
@@ -13,6 +15,15 @@ public class BlockLockedChest extends Block {
     }
 
     public void a(World world, int i, int j, int k, Random random) {
+        // CraftBukkit start
+        LockedChestDecayEvent event = new LockedChestDecayEvent(world.getWorld().getBlockAt(i, j, k));
+        world.getServer().getPluginManager().callEvent(event);
+
+        if (event.isCancelled()) {
+            return;
+        }
+        // CraftBukkit end
+
         world.setAir(i, j, k);
     }
 }
